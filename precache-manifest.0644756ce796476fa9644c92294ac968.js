402bb11eb87926c91fae363c1c8f01756f4f7ff3
ankitbapat <41078470+ankitbapat@users.noreply.github.com>
2020-08-20 12:41:21 -0400

    Updates

---
+++ precache-manifest.0644756ce796476fa9644c92294ac968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2453452dfdd3760b2297590de293788b",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "1ec45df13d6486b6203b",
+    "url": "/covid19/static/css/main.e1777c2e.chunk.css"
+  },
+  {
+    "revision": "d271b5b23d6c45004dec",
+    "url": "/covid19/static/js/2.510dfae0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.510dfae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec45df13d6486b6203b",
+    "url": "/covid19/static/js/main.947d9b03.chunk.js"
+  },
+  {
+    "revision": "3f9ea9de9b45ec0fb219",
+    "url": "/covid19/static/js/runtime-main.59095949.js"
+  }
+]);
